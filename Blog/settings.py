ba5c03e731380803b9d72308297d6ecc78369ccc
santuzzxooy <santiagorochav@gmail.com>
2023-05-22 21:59:11 -0500

    cambios_vercel

--- Blog/settings.py
+++ Blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z#*drz00v$nbwms*r%2l@t#^or^qsd6%5!zdmg9#n@^7=il%zh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Blog.wsgi --log-file -
